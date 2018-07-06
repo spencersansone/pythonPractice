f4658d9541dcf608b31aa6b9337ce5cbe5543886
Spencer Sansone <spencersemail101@gmail.com>
2018-07-05 23:03:32 -0400

    improve sendGmailSingleRecipient.py

--- sendGmailSingleRecipient.py
+++ sendGmailSingleRecipient.py
@@ -1,51 +1,34 @@
-#required smtp library
 import smtplib
+from email.mime.text import MIMEText
 
-def singleRecipientEmail(info):
-    #formatted email message variable using sender email,
-	#	recipient email, subject, and body
-    email_text = """
-    From: {}  
-    To: {}  
-    Subject: {}
-
-    {}
-    """.format(info[0], info[2], info[3], info[4])
-    
-    
+def sendGmail(info):
+    msg = MIMEText(info['b'])
+    msg['Subject'] = info['s']
+    msg['From'] = info['g_sender']
+    msg['To'] = info['r_email']
     try:
-		#smtp server info setup 
+        print("Attempting to send, please wait...")
         server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
-		
-		#test server contact
         server.ehlo()
-		
-		#attempt login
-        server.login(info[0], info[1])
-		
-		#attempt to send message with sender email, recipient
-		#email and formatted email message
-        server.sendmail(info[0], info[2], email_text)
-		
-		#close connection
+        server.login(info['g_sender'], info['g_pass'])
+        server.sendmail(info['g_sender'], info['r_email'], msg.as_string())
         server.close()
-
         print('Email sent!')
     except:
-		#if server test, login attempt or send attempt fails...
-		
         print('Something went wrong...')
 
-#ask for needed information
-gmail_email = input("Please enter your gmail address: ")
-gmail_password = input("Please enter your gmail password: ")
+gmail_sender = input("Please enter your gmail address: ")
+gmail_pass = input("Please enter your gmail password: ")
 recipient_email = input("Please enter the recipient's email address: ")
 subject = input("Please enter the subject: ")
 body = input("Please enter the message: ")
 
-#email info array
-#           0              1               2           3    4
-info = [gmail_email,gmail_password,recipient_email,subject,body]
+info = {}
+info['g_sender'] = gmail_sender
+info['g_pass'] = gmail_pass
+info['r_email'] = recipient_email
+info['s'] = subject
+info['b'] = body
 
-#utilize function
-singleRecipientEmail(info)
+sendGmail(info)
+
