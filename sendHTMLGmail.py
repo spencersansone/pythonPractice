02d27294b2e849f18963ca5065ed94f98ad6654a
Spencer Sansone <spencersemail101@gmail.com>
2018-07-06 21:59:26 -0400

    add sendHTMLGmail.py

---
+++ sendHTMLGmail.py
@@ -0,0 +1,34 @@
+import smtplib
+from email.mime.text import MIMEText
+
+def sendGmail(info):
+    msg = MIMEText(u'{}'.format(info['b']),'html')
+    msg['Subject'] = info['s']
+    msg['From'] = info['g_sender']
+    msg['To'] = info['r_email']
+    try:
+        print("Attempting to send, please wait...")
+        server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+        server.ehlo()
+        server.login(info['g_sender'], info['g_pass'])
+        server.sendmail(info['g_sender'], info['r_email'], msg.as_string())
+        server.close()
+        print('Email sent!')
+    except:
+        print('Something went wrong...')
+
+gmail_sender = input("Please enter your gmail address: ")
+gmail_pass = input("Please enter your gmail password: ")
+recipient_email = input("Please enter the recipient's email address: ")
+subject = input("Please enter the subject: ")
+body = input("Please enter the HTML message: ")
+
+info = {}
+info['g_sender'] = gmail_sender
+info['g_pass'] = gmail_pass
+info['r_email'] = recipient_email
+info['s'] = subject
+info['b'] = body
+
+sendGmail(info)
+
