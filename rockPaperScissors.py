c92d7f16375696c45834eaadee10dcb567075eb0
Spencer Sansone <spencersemail101@gmail.com>
2018-07-08 13:59:37 -0400

    improve rockPaperScissors.py

--- rockPaperScissors.py
+++ rockPaperScissors.py
@@ -13,29 +13,24 @@ def result(userChoice,computerChoice):
             return "Computer wins!"
         else:
             return "User wins!"
-    elif userChoice == "S":
+    else:
         if computerChoice == "R":
             return "Computer wins!"
         else:
             return "User wins!"
 
 keepGoing = True
+choicesArray = ["R","P","S"]
 
 while(keepGoing):
-    b = True
-    choicesArray = ["R","P","S"]
-
-    
-
-    while(b):
+    while(True):
         userChoice = input("Rock, paper or scissors? (R/P/S):").upper()
-        if userChoice.upper() not in choicesArray:
+        if userChoice not in choicesArray:
             print("Please try again")
         else:
            break
 
     computerChoice = choicesArray[randint(0,2)]
-
     r_string = result(userChoice,computerChoice)
 
     print("""
